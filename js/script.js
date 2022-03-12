0e36909cedb225cfd407e3e9d6a79d7060c504c8
Mateusz Gajewski <101431755+matt-gski@users.noreply.github.com>
2022-03-12 18:35:34 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Jr. Full-Stack", "Video Editor", "Discord Bot Developer"],
+    strings: ["jr. frontend developer", "aspiring full-stack developer"],
     typeSpeed: 60,
     startDelay: 900,
     showCursor: false,
